669469cd6687ae52ceec98fd8dcc203adc382abc
Twily <reveuse1204@gmail.com>
2017-06-14 10:45:06 +0900

    updated in school

--- app/src/main/java/com/example/reveu/twilycalendar/FragCalendarMonth.java
+++ app/src/main/java/com/example/reveu/twilycalendar/FragCalendarMonth.java
@@ -1,18 +1,14 @@
 package com.example.reveu.twilycalendar;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.Calendar;
-import java.util.HashMap;
 
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
